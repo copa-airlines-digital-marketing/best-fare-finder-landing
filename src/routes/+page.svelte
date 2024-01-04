e9f048f84148d9be56f0007676f69877ef0b8036
josevch <javch1995@gmail.com>
2024-01-04 12:07:44 -0500

    Init for FR landing

---
+++ src/routes/+page.svelte
@@ -0,0 +1,53 @@
+<div class="bffHeader">
+    <div class="bffHeader__imageSet">
+        <div class="bffHeader__carousel ">
+            <picture class="d-appear-1">
+                <source media="(min-width: 1024px)"
+                    srcset="https://www.copaair.com/promotions/destinations/img/bff-hero-1.jpg" class="" />
+                <source media="(min-width: 768px)"
+                    srcset="https://www.copaair.com/promotions/destinations/img/bff-hero-mobile-1024-1.jpg"
+                    class="" /> <img alt=""
+                    src="https://www.copaair.com/promotions/destinations/img/bff-hero-mobile-1080-1.jpg"
+                    class="bffHeader__image re-item" rel="" /> </picture>
+            <picture class="d-appear-2 ">
+                <source media="(min-width: 1024px)"
+                    srcset="https://www.copaair.com/promotions/destinations/img/bff-hero-2.jpg" class="" />
+                <source media="(min-width: 768px)"
+                    srcset="https://www.copaair.com/promotions/destinations/img/bff-hero-mobile-1024-2.jpg"
+                    class="" /> <img alt=""
+                    src="https://www.copaair.com/promotions/destinations/img/bff-hero-mobile-1080-2.jpg"
+                    class="bffHeader__image " rel="" /> </picture>
+            <picture class="d-appear-3">
+                <source media="(min-width: 1024px)"
+                    srcset="https://www.copaair.com/promotions/destinations/img/bff-hero-3.jpg" class="" />
+                <source media="(min-width: 768px)"
+                    srcset="https://www.copaair.com/promotions/destinations/img/bff-hero-mobile-1024-3.jpg"
+                    class="" /> <img alt=""
+                    src="https://www.copaair.com/promotions/destinations/img/bff-hero-mobile-1080-3.jpg"
+                    class="bffHeader__image " rel="" /> </picture>
+            <picture class="">
+                <source media="(min-width: 1024px)"
+                    srcset="https://www.copaair.com/promotions/destinations/img/bff-hero-4.jpg" class="" />
+                <source media="(min-width: 768px)"
+                    srcset="https://www.copaair.com/promotions/destinations/img/bff-hero-mobile-1024-4.jpg"
+                    class="" /> <img alt=""
+                    src="https://www.copaair.com/promotions/destinations/img/bff-hero-mobile-1080-4.jpg"
+                    class="bffHeader__image " rel="" /> </picture>
+        </div>
+    </div>
+    <div class="bffHeader__overlay"></div>
+    <article class="bffHeader__logo"><img class="bffHeader__logo__set"
+            src="https://www.copaair.com/promotions/destinations/img/best-fare-finder-logo-white.svg"
+            alt="Best Fare Finder" /></article>
+    <div class="bffHeader__selector" id="originSelector">
+        <form id="odForm" action="" method="POST">
+            <div class="selector ">
+                <h2 class="pointerH2">Tarifas desde:<select name="origins" class="selectOriDest"
+                        id="origin-selector"></select>a<select name="destinations"
+                        class="selectOriDest selectDestOnly" id="destination-selector"></select></h2>
+            </div>
+            <div class="inputShort"><input class="goBtn" type="submit" value="Buscar tarifas" /></div>
+        </form>
+        <h3 id="showStopper" class="erro ">Invalid Route!</h3>
+    </div>
+</div>
